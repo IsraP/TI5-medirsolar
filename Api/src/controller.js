849c7bf553b5ed46c9cd908cfc41305b3ca95759
Samuel <samuelmsifuentes@gmail.com>
2022-05-26 20:55:24 -0300

    comit-api

--- Api/src/controller.js
+++ Api/src/controller.js
@@ -1,52 +1,96 @@
 const connection = require('./database')
 
 module.exports = {
-    async index (request,response){
-        const valores = await connection.pool.query('select * from medicao')
-        return response.json(
-            valores
-        );
-    },
-    async MinMaxAtual (request,response){
-        const{unidade = 's'} = request.query;
-        var hj =new Date (Date.now());
-            hj.setHours(-3,0,0,0)
-        const valores = await connection.pool.query(`select * from medicao where data > 
-        '${hj.toISOString()}' AND unidade = '${unidade}' order by data desc`)
-        if(valores.length > 0){
-            
+    async index(request, response) {
+        const { data = null } = request.query;
+        console.log(data);
+        if ( data == null || data == '') {
+            const valores = await connection.pool.query('select * from medicao')
+            return response.json(
+                valores
+            );
+        } else {
+            const data2 = new Date(data);
+            const valores = await connection.pool.query(`select * from medicao where data > '${data2.toISOString()}'`)
+            return response.json(
+                valores
+            );
+        }
         
+    },
+    async FromUnidade(request, response) {
+        const { unidade = 's', data = null } = request.query;
+        if ( data == null || data == '') {
+            const valores = await connection.pool.query(`select * from medicao where unidade = '${unidade}' `)
+            return response.json(
+                valores
+            );
+        } else {
+            const data2 = new Date(data);
+            const valores = await connection.pool.query(`select * from medicao where unidade = '${unidade}' AND data > '${data2.toISOString()}' `)
+            return response.json(
+                valores
+            );
+        }
+    },
+    async MinMaxAtual(request, response) {
+        const { unidade = 's' } = request.query;
+        var hj = new Date(Date.now());
+        hj.setHours(-3, 0, 0, 0)
+        const valores = await connection.pool.query(`select * from medicao 
+        where data > '${hj.toISOString()}' 
+        AND unidade = '${unidade}' order by data desc`)
+        if (valores.length > 0) {
+
+
             const Metricas = {
                 tempUmiAtual: valores[0],
-                tempMaior:null,
-                tempMenor:null,
-                umiMaior:null,
-                umiMenor:null
+                tempMaior: null,
+                tempMenor: null,
+                umiMaior: null,
+                umiMenor: null
             }
             valores.sort((c1, c2) => (c1.temperatura < c2.temperatura) ? 1 : (c1.temperatura > c2.temperatura) ? -1 : 0)
-            Metricas.tempMaior= valores[0];
-            Metricas.tempMenor= valores[valores.length -1];
+            Metricas.tempMaior = valores[0];
+            Metricas.tempMenor = valores[valores.length - 1];
             valores.sort((c1, c2) => (c1.umidade < c2.umidade) ? 1 : (c1.umidade > c2.umidade) ? -1 : 0)
-            Metricas.umiMaior= valores[0];
-            Metricas.umiMenor= valores[valores.length -1];
+            Metricas.umiMaior = valores[0];
+            Metricas.umiMenor = valores[valores.length - 1];
             return response.json(
                 Metricas
             );
-        }else{
+        } else {
             return response.json(
                 null
             );
         }
     },
-    async Unidades(request,response){
+    async Unidades(request, response) {
         const valores = await connection.pool.query(`select unidade from medicao group by unidade`)
         return response.json(
             valores
         );
     },
-    async FromUnidade (request,response){
-        const{unidade = 's'} = request.query;
-        const valores = await connection.pool.query(`select * from medicao where unidade = '${unidade}' `)
+
+    async Intevalo(request, response) {
+        const { dias = 1 } = request.query;
+        var hj = new Date(Date.now());
+        var dataFim = new Date(Date.now());
+        hj.setHours(-3, 0, 0, 0)
+        dataFim.setHours(-3, 0, 0, 0)
+        dataFim.setDate(hj.getDate() - dias);
+        console.log(hj.toISOString());
+
+        const valores = await connection.pool.query(`
+            SELECT MAX(temperatura) as tempMaior, 
+            MIN(temperatura) as tempMenor, 
+            MAX(umidade) as umiMaior, 
+            MIN(umidade) as umiMenor, 
+            DATE(data) as dia
+            from medicao
+            WHERE data > '${dataFim.toISOString()}'
+            group by DATE(data) , unidade`
+        )
         return response.json(
             valores
         );
--- Api/src/routes.js
+++ Api/src/routes.js
@@ -4,20 +4,36 @@ const {celebrate,Segments,Joi} = require('celebrate')
 const Controller = require('./controller')
 
 const routes = express.Router();
+//traz todos os registros do banco
+routes.get('/getAll',celebrate({
+    [Segments.QUERY] :Joi.object().keys({
+        data: Joi.string()
+    })
+ }), Controller.index)
 
-routes.get('/getAll', Controller.index)
-
+//traz o maior o menor e o atual do dia de hj
 routes.get('/getMaxMin',celebrate({
     [Segments.QUERY] :Joi.object().keys({
         unidade: Joi.string(),
     })
  }), Controller.MinMaxAtual)
+
+// traz a lista de unidades
 routes.get('/getUnidades', Controller.Unidades)
 
+//traz todos os registros de uma unidade
 routes.get('/getAllFromUnidade',celebrate({
     [Segments.QUERY] :Joi.object().keys({
         unidade: Joi.string(),
+        data: Joi.string()
     })
  }),Controller.FromUnidade);
+
+//traz o maior e o menor de todos os dias ate a hj - dias
+routes.get('/getAllFromData',celebrate({
+    [Segments.QUERY] :Joi.object().keys({
+        dias: Joi.number(),
+    })
+ }),Controller.Intevalo);
 
 module.exports = routes;
