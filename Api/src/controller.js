2af4525ccf993c776409d30e0b1490fb8d547622
Samuel <samuelmsifuentes@gmail.com>
2022-05-26 00:23:23 -0300

    mais um bocado

--- Api/src/controller.js
+++ Api/src/controller.js
@@ -2,15 +2,62 @@ const connection = require('./database')
 
 module.exports = {
     async index (request,response){
-        console.log("aaaa");
-        const valores = await connection.then(async (res)=>{
-            console.log(res);
-            var a = await res.query('select * from medicao')
-            return response.json(
-                a
-            );
-        })
-        
+        const valores = await connection.pool.query('select * from medicao')
+        return response.json(
+            valores
+        );
     },
- 
+    async MinMaxAtual (request,response){
+        const{unidade = 's'} = request.query;
+        var hj =new Date (Date.now());
+            hj.setHours(-3,0,0,0)
+        const valores = await connection.pool.query(`select * from medicao where data > 
+        '${hj.toISOString()}' AND unidade = '${unidade}' order by data desc`)
+        if(valores.length > 0){
+            
+        
+            const Metricas = {
+                tempUmiAtual: valores[0],
+                tempMaior:null,
+                tempMenor:null,
+                umiMaior:null,
+                umiMenor:null
+            }
+            valores.sort((c1, c2) => (c1.temperatura < c2.temperatura) ? 1 : (c1.temperatura > c2.temperatura) ? -1 : 0)
+            Metricas.tempMaior= valores[0];
+            Metricas.tempMenor= valores[valores.length -1];
+            valores.sort((c1, c2) => (c1.umidade < c2.umidade) ? 1 : (c1.umidade > c2.umidade) ? -1 : 0)
+            Metricas.umiMaior= valores[0];
+            Metricas.umiMenor= valores[valores.length -1];
+            return response.json(
+                Metricas
+            );
+        }else{
+            return response.json(
+                null
+            );
+        }
+    },
+    async Unidades(request,response){
+        const valores = await connection.pool.query(`select unidade from medicao group by unidade`)
+        return response.json(
+            valores
+        );
+    },
+    async FromUnidade (request,response){
+        const{unidade = 's'} = request.query;
+        const valores = await connection.pool.query(`select * from medicao where unidade = '${unidade}' `)
+        return response.json(
+            valores
+        );
+    },
 }
+
+
+// {
+//     "id": 2,
+//     "temperatura": 0,
+//     "umidade": 22,
+//     "data": "2022-05-25T04:36:55.000Z",
+//     "unidade": "S"
+// }
--- Api/src/database.js
+++ Api/src/database.js
@@ -1,66 +1,20 @@
 const mariadb = require('mariadb');
 const { Client } = require('ssh2');
 const sshClient = new Client();
-const tunnelConfig = {
-    host: '187.20.157.119',
-    port: '13777',
-    username: 'pi',
-    password: 'raissalinda'
-}
-const forwardConfig = {
+// const tunnelConfig = {
+//     host: '187.20.157.119',
+//     port: '13777',
+//     username: 'pi',
+//     password: 'raissalinda'
+// }
+const pool = mariadb.createPool( {
     host: '127.0.0.1', 
     port:'3306', // 13777
     user:'root', 
     password: 'raissalinda',
     database:'TI5',
     connectionLimit: 5
-}
-
-const SSHConnection = new Promise((resolve, reject) => {
-    sshClient.on('ready', () => {
-        console.log("222");
-        sshClient.forwardOut(
-        "127.0.0.1",
-        forwardConfig.port,
-        forwardConfig.host,
-        forwardConfig.port,
-        async (err, stream) => {
-            console.log("maria");
-             if (err) reject(err);
-            
-            const pool = mariadb.createPool(forwardConfig)
-            console.log("pool aaaa ",pool);
-            const conn = await pool.getConnection();
-            const a = await conn.query('select * from medicao')
-            console.log("Amem ",a);
-                resolve(pool);   
-                
-       });
-    }).connect(tunnelConfig);
+})
+module.exports = Object.freeze({
+    pool: pool
 });
-module.exports = SSHConnection;
-// const pool 
-// 
-
-// pool.getConnection()
-//     .then(conn => {
-    
-//       conn.query("SELECT 1 as val")
-//         .then((rows) => {
-//           console.log(rows); //[ {val: 1}, meta: ... ]
-//           //Table must have been created before 
-//           // " CREATE TABLE myTable (id int, val varchar(255)) "
-//           return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-//         })
-//         .then((res) => {
-//           console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-//           conn.end();
-//         })
-//         .catch(err => {
-//           //handle error
-//           console.log(err); 
-//           conn.end();
-//         })
-//     }).catch(err => {
-//       //not connected
-//     });
