74d697ec02be8bce941412b050114ced187ff009
Samuel <samuelmsifuentes@gmail.com>
2022-05-27 00:49:47 -0300

    aaaaaa

--- Api/src/app.js
+++ Api/src/app.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors())
-process.env.TZ = 'America/Sao_Paulo'; // UTC +00:00
-console.log(new Date().toString())
+
 
 module.exports = app;
--- Api/src/server.js
+++ Api/src/server.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const port = 3000
-
+process.env.TZ = 'America/Sao_Paulo'; // UTC +00:00
+console.log(new Date().toString())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
